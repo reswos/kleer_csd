ca46b0741fbb906cdf743c20b87f6790f3a9a289
Sergio Gonzalez <sergio.gonzalez1@astrazeneca.com>
2020-11-17 17:22:22 -0600

    Agrega nuevo parametro (precio) a la funcion generar factura

--- actividad_1/factura/factura.rb
+++ actividad_1/factura/factura.rb
@@ -1,11 +1,12 @@
 class Factura
 
-    def generar_factura(cantidad)
-        puts "# #{cantidad}"
+    def generar_factura(cantidad, precio)
+        puts "# #{cantidad} #{precio}"
     end
 end
 
 factura = Factura.new
-factura.generar_factura(ARGV[0])
+factura.generar_factura(ARGV[0],ARGV[1])
 
 #Construir una clase que reciba 1 parametro (Cantidad) un metodo generar factura y lo imprima
+#Agregar a la clase que reciba 1 parametro extra (Precio Unitario)
