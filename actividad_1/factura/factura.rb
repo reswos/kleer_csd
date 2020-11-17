920f9764cb862fa5432a145765259e0d8878114d
Sergio Gonzalez <sergio.gonzalez1@astrazeneca.com>
2020-11-17 17:25:01 -0600

    Agrega a la funcion generar factura el resultado del precio bruto

--- actividad_1/factura/factura.rb
+++ actividad_1/factura/factura.rb
@@ -1,7 +1,7 @@
 class Factura
 
     def generar_factura(cantidad, precio)
-        puts "# #{cantidad} #{precio}"
+        puts "# #{cantidad} * #{precio} = #{cantidad.to_f * precio.to_f}"
     end
 end
 
@@ -10,3 +10,4 @@ factura.generar_factura(ARGV[0],ARGV[1])
 
 #Construir una clase que reciba 1 parametro (Cantidad) un metodo generar factura y lo imprima
 #Agregar a la clase que reciba 1 parametro extra (Precio Unitario)
+#Agregar a la funcion generar factura la impresion  que regrese e imprima el precio unitario por la cantidad
